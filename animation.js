6b1f47bbaa482c97c911a841023fcad70019031a
Rami Hadey <Rhadey216@west-mec.org>
2021-04-09 15:27:47 -0700

    Added a nav & fading

---
+++ animation.js
@@ -0,0 +1,15 @@
+//Nav bar animation
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+
+//landing page fade in
+function myFunction() {
+  location.reload();
+}
