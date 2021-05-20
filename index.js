40d28dedd534f8326c69f15c56397f36bf91a534
Rami Hadey <Rami3446@gmail.com>
2021-05-20 15:25:36 -0700

    resized for mobile

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ var el = document.getElementById('str');
 str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
 var running = setTimeout(animate, 90);
 })();
+
+var string2 = "I am an aspiring web and software developer";
+var str2 = string2.split("");
+var el2 = document.getElementById('str2');
+(function animate() {
+str2.length > 0 ? el2.innerHTML += str2.shift() : clearTimeout(running2); 
+var running2 = setTimeout(animate, 90);
+})();
